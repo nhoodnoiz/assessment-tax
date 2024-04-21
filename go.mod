3be9e44a11dc179e345143a5004c828e14b8ab86
North <natdanai.u@gmail.com>
2024-04-22 01:43:39 +0700

    first commit main branch

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/KKGo-Software-engineering/assessment-tax
 
 go 1.21.9
 
-require github.com/labstack/echo/v4 v4.11.4
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.11.4
+	github.com/lib/pq v1.10.9
+)
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -14,4 +19,5 @@ require (
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,92 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
+// type Taxes struct{
+// 	totalIncome float64 ``
+// }
+
+var db *sql.DB
+
 func main() {
+
+	// db connection
+	var err error
+
+	psqlInfo := os.Getenv("DATABASE_URL")
+
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, username, password, databaseName)
+
+	db, err = sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal("Connect to database error ", err)
+	}
+	defer db.Close()
+
+	// Run only first time
+	// createTb := `
+	// CREATE TABLE IF NOT EXISTS taxes (
+	// 	id SERIAL PRIMARY KEY, title TEXT,
+	// 	amount FLOAT,
+	// 	note TEXT,
+	// 	tags TEXT[]);
+	// `
+	// createTb := `
+	// CREATE TABLE IF NOT EXISTS taxes(
+	// 	totalIncome FLOAT,
+	// 	wht FLOAT,
+
+	// )
+	// `
+
+	// _, err = db.Exec(createTb)
+
+	// if err != nil {
+	// 	log.Fatal("can't create table", err)
+	// }
+	// fmt.Println("create table success")
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	// e.GET("/health", healthHandler)
+
+	adminUsername := os.Getenv("ADMIN_USERNAME")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == adminUsername && password == adminPassword {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	e.GET("/health", healthHandler)
+	// e.GET("/users", getUsersHandler)
+
+	// Start http server
+	port := os.Getenv("PORT")
+	e.Logger.Fatal(e.Start(":" + port))
 }
+
+func healthHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, Go Bootcamp!")
+}
+
+// func getUsersHandler(c echo.Context) error {
+
+// }
